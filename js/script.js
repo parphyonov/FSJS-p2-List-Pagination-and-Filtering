579f2a7e77a0b41e3b7efc8bd9975d659788d952
Alexei Parphyonov <alexei.s.parphyonov@gmail.com>
2018-09-30 20:20:54 +0300

    Refactor the whole app, some code were outframed into separate functions and overall, now it is read and understood better

--- js/script.js
+++ js/script.js
@@ -6,16 +6,26 @@ FSJS project 2 - List Filter and Pagination
 
 // The list of .student-item-s as our primary pagination target
 const students = document.querySelectorAll('.student-item');
-students.forEach(student => {
-  student.style.display = 'none';
-});
+
+// A function to hide all .student-item-s from the view
+// We will need it a couple of times
+const hideAllStudents = () => {
+  students.forEach(student => {
+    student.style.display = 'none';
+  });
+};
+// And it is the first time
+hideAllStudents();
+
 // .page div will have some children appended later
 const page = document.querySelector('.page');
+// Page header will be used to insert search field
+const pageHeader = document.querySelector('.page-header');
+
 // This variable sets the number of items to be shown on the page
 // FEEL FREE TO CHANGE IT for our paginator to display another number of items per page
 const paginationBreaker = 10;
-// Page header will be used to insert search field
-const pageHeader = document.querySelector('.page-header');
+
 
 
 // The function accepts a list and a page number and uses CSS properties
@@ -25,7 +35,7 @@ const showPage = (list, page) => {
   // These variables set the top and bottom range of possible indices
   // E.g. page 3 results in 29 here
   const bottomElem = page * paginationBreaker - 1;
-  // ...and 20 here. After all these are INDECES, not actual ordinar places of .student.item
+  // ...and 20 here. After all these are INDICES, not actual ordinar places of .student.item
   const topElem = bottomElem - 9;
 
   // Loop through the array, check if a student's index within the page's boundaries and...
@@ -41,14 +51,42 @@ const showPage = (list, page) => {
 
 };
 
-// Create and append the pagination links
-const appendPageLinks = list => {
-
-  // Removes pagination if it already exists
+// This function deletes already existing pagination from the page
+// It will be called from within functions that add new pagination or
+// do the search on the items and thus needs old paginations gone.
+const deletePagination = () => {
   const oldPages = document.querySelector('.pagination');
   if (oldPages) {
     oldPages.remove();
   }
+}
+
+// !!! ::: Functionality ::: !!!
+// I decided that doing it all in one function is contraditing with at least
+// the next function name (appendPageLinks)
+const addPaginationBehavior = (list, paginationDiv) => {
+  // I had first introduced functionality through applying listener to each anchor,
+  // but then decided to implement this as it was more challenging for me
+  // to figure our how and took some time.
+  paginationDiv.addEventListener('click', event => {
+    const page = event.target;
+    const pageToShow = parseInt(page.textContent);
+    showPage(list, pageToShow);
+    // I did not like this way, but if it is not a listener of each a,
+    // it appeared to me to be the shortest valid solution
+    // without jQuery calls (which I did not want to use in this project)
+    document.querySelectorAll('.pagination a').forEach(anchor => {
+      anchor.className = '';
+    });
+    // Now we only add active class to the clicked anchor
+    page.className = 'active';
+  });
+}
+
+// Create and append the pagination links
+const appendPageLinks = list => {
+
+  deletePagination();
 
   // This determines how many pages will be needed
   // Math.ceil is used in case the division provides a float, and in this case an extra page is needed
@@ -79,26 +117,43 @@ const appendPageLinks = list => {
     ul.appendChild(li);
   }
 
-  // !!! ::: Functionality ::: !!!
-
-  // I had first introduced functionality through applying listener to each anchor,
-  // but then decided to implement this as it was more challenging for me
-  // to figure our how and took some time.
-  paginationDiv.addEventListener('click', event => {
-    const page = event.target;
-    const pageToShow = parseInt(page.textContent);
-    showPage(list, pageToShow);
-    // I did not like this way, but if it is not a listener of each a,
-    // it appeared to me to be the shortest valid solution
-    // without jQuery calls (which I did not want to use in this project)
-    document.querySelectorAll('.pagination a').forEach(anchor => {
-      anchor.className = '';
-    });
-    // Now we only add active class to the clicked anchor
-    page.className = 'active';
-  });
+  // Oh, these infinite number of function calls
+  addPaginationBehavior(list, paginationDiv);
 
 };
+
+const addSearchCapability = (button, input, list) => {
+  // On click onto button we make a search on the elements
+  button.addEventListener('click', () => {
+    // All students names are lower case, so I want to make sure anything a user inputs is also lower case
+    const currentSearch = input.value.toLowerCase();
+    // This array will hold the results that match the search criteria
+    const customSearch = [];
+    // We loop through each of the student's items
+    list.forEach(listElem => {
+      // Getting hold on each student's name
+      const elemName = listElem.firstElementChild.children[1].textContent;
+      // If it contains a search text...
+      if (elemName.includes(currentSearch)) {
+        // ...it adds to the search results array
+        customSearch.push(listElem);
+      };
+    });
+    // We hide every student from the page to show search results
+    hideAllStudents();
+
+    // Some logic, after all
+    if (customSearch.length === 0) {
+      deletePagination();
+      // Displays a message that no results were found, click on a link to refresh the page
+      const errorMessage = `<h2>No students in our database met your criteria. Please, <a class="refresh" onclick="location.reload();">reload</a> and try a different search!</h2>`;
+      pageHeader.innerHTML = errorMessage;
+    } else {
+      showPage(customSearch, 1);
+      appendPageLinks(customSearch);
+    }
+  });
+}
 
 // Appends search field to the top of the web page
 const appendSearchField = () => {
@@ -121,22 +176,9 @@ const appendSearchField = () => {
   // The search field is displayed, but no functionality YET
   pageHeader.appendChild(studentSearch);
 
-  // On click onto button we make a search on the elements
-  button.addEventListener('click', () => {
-    const currentSearch = input.value.toLowerCase();
-    const customSearch = [];
-    students.forEach(student => {
-      const studentName = student.firstElementChild.children[1].textContent;
-      if (studentName.includes(currentSearch)) {
-        customSearch.push(student);
-      };
-    });
-    students.forEach(student => {
-      student.style.display = 'none';
-    });
-    showPage(customSearch, 1);
-    appendPageLinks(customSearch);
-  });
+  // And let the search commence...
+  addSearchCapability(button, input, students);
+
 };
 
 appendSearchField();
